a1c0044fc65045dd8d471a2d02cf52823bf5b6ba
Ryan Walker <ryan.walker@tombola.com>
2019-10-19 00:19:56 +0100

    FIXED ALL THE BUGS
    Plus rocket can now be destroyed part by part until there is nothing left.

---
+++ Classes/rocket.js
@@ -0,0 +1,184 @@
+// Attachments is a bool array showing if the object can attach on a certain side [up, right, down, left]
+
+class Part {
+    constructor(spriteName, attachments, mass) {
+        this.sprite;
+        this.parent;
+        this.spriteName = spriteName;
+        this.attachments = attachments;
+        this.mass = mass;
+    }
+
+    addPart(x, y, parent) {
+        this.parent = parent;
+        this.sprite = new Sprite(id[this.spriteName]);
+        this.sprite.position.set(x, y);
+        this.sprite.scale.set(1 / 16);
+        this.parent.addChild(this.sprite);
+    }
+
+    removePart() {
+        this.parent.removeChild(this.sprite);
+    }
+}
+
+class FuelTank extends Part {
+    constructor(spriteName, attachments, mass, fuel) {
+        super(spriteName, attachments, mass);
+        this.fuel = fuel;
+    }
+}
+
+class Engine extends Part {
+    constructor(spriteName, attachments, mass, thrust) {
+        super(spriteName, attachments, mass);
+        this.thrust = thrust;
+    }
+}
+
+class Cabin extends Part {
+    constructor(spriteName, attachments, mass) {
+        super(spriteName, attachments, mass);
+    }
+}
+
+class Rocket {
+    constructor(x, y, width, height) {
+        this.container = new Container();
+
+
+        this.width = width;
+        this.height = height;
+        this.parts = [
+            [width],
+            [height]
+        ];
+        this.parts = [...Array(width)].map(e => Array(height).fill(undefined));
+
+        this.mass;
+        this.COM;
+        this.fuel;
+        this.fuelMax;
+        this.thrust;
+
+        this.position = new Vector(x, y);
+        this.rotation = 0;
+        this.velocity = Vector.zero;
+        this.angularVelocity = 0;
+        this.scale = 16 * 8;
+
+        this.move();
+    }
+
+    move() {
+        this.position = this.position.add(this.velocity);
+        this.rotation += this.angularVelocity;
+        this.container.position.set(this.position.x, this.position.y);
+        this.container.rotation = this.rotation;
+        this.container.scale.set(this.scale);
+    }
+
+    placePart(part, x, y) {
+        if (x < 0 || y < 0 || x >= this.parts.length || y >= this.parts[x].length) {
+            return;
+        }
+        if (this.parts[x][y] === undefined) {
+            this.parts[x][y] = Object.assign(new part.constructor(), part);
+            this.parts[x][y].addPart(x, y, this.container);
+        } else {
+            this.parts[x][y].removePart();
+            this.parts[x][y] = undefined;
+        }
+        this.calculateProperties();
+    }
+
+    calculateProperties() {
+        this.mass = 0;
+        this.fuel = 0;
+        this.thrust = 0;
+
+        this.parts.map(y =>
+            y.map(part => {
+                if (part !== undefined) {
+                    this.mass += part.mass;
+
+                    if (part.constructor.name === "FuelTank") {
+                        this.fuel += part.fuel;
+                        this.fuelMax = this.fuel;
+                    } else if (part.constructor.name === "Engine") {
+                        this.thrust += part.thrust;
+                    }
+                }
+            })
+        );
+
+        let mx = 0,
+            my = 0;
+        for (let x = 0; x < this.parts.length; x++) {
+            for (let y = 0; y < this.parts[0].length; y++) {
+                const part = this.parts[x][y];
+                if (part !== undefined) {
+                    mx += part.mass * x;
+                    my += part.mass * y;
+                    if (y > this.height)
+                        this.height = y + 1;
+                }
+            }
+        }
+        if (this.mass != 0)
+            this.COM = new Vector(mx / this.mass, my / this.mass);
+
+        // console.clear();
+        // console.log(`Mass: ${this.mass} Fuel: ${this.fuel} Thrust: ${this.thrust}`);
+        // console.log(`Thrust to Weight: ${this.thrust / this.mass}`);
+    }
+
+    worldPos(value) {
+        return this.position.add(Vector.zero.toVector(value).add(Vector.half).rotate(this.rotation).multiply(this.scale))
+    }
+
+
+
+    localPos(value) {
+        let pos = value
+            .subtract(this.position)
+            .rotate(-this.rotation)
+            .divide(this.scale);
+        //console.log(pos.floor());
+        return pos;
+    }
+
+    checkCollisions() {
+        let partCount = 0;
+        for (let x = 0; x < this.parts.length; x++) {
+            for (let y = 0; y < this.parts[0].length; y++) {
+                if (this.parts[x][y] !== undefined) {
+                    partCount++;
+                    if (this.worldPos(this.parts[x][y].sprite).distance(planet) <= planet.radius + 2 && alive) {
+                        this.parts[x][y].removePart();
+                        this.parts[x][y] = undefined;
+                        this.changeCOM();
+                    }
+                }
+            }
+        }
+        if (partCount <= 0) {
+            alive = false;
+        }
+    }
+
+    changeCOM() {
+        let prevCOM = this.COM;
+        this.calculateProperties();
+        let COMDiff = prevCOM.subtract(this.COM);
+        this.position = this.position.subtract(COMDiff.multiply(4).rotate(this.rotation));
+        for (let x = 0; x < this.parts.length; x++) {
+            for (let y = 0; y < this.parts[0].length; y++) {
+                if (this.parts[x][y] !== undefined) {
+                    this.parts[x][y].sprite.x += COMDiff.x;
+                    this.parts[x][y].sprite.y += COMDiff.y;
+                }
+            }
+        }
+    }
+}
---
+++ functions.js
@@ -0,0 +1,165 @@
+function trajectory(position, velocity, maxIterations, scale) {
+    var points = [],
+        vel = [],
+        length = 0;
+    (fullOrbit = gravity(position).toRad() + Math.PI),
+    (currentAngle = 0),
+    (looped = false),
+    (playerDistance = position.distance(planet)),
+    (lastDistance = Vector.zero),
+    (apoapsis = Vector.zero),
+    (apoapsisDistance = 0);
+    periapsis = Vector.zero;
+    periapsisDistance = 0;
+
+    scale /= Math.pow(maxIterations, 1.5);
+
+    points[0] = position;
+    vel[0] = velocity;
+
+    for (let i = 1; i < maxIterations; i++) {
+        let gravForce = gravity(points[i - 1]);
+        lastDistance = points[i - 1].distance(planet);
+        length = scale * Math.pow(lastDistance, 1.5);
+
+        if (gravForce != Vector.zero) {
+            collisionCourse = false;
+            if (currentAngle > fullOrbit) {
+                looped = true;
+            }
+
+            if (looped && currentAngle < fullOrbit + 0.1) {
+                points[i] = position;
+                break;
+            } else {
+                currentAngle = gravForce.toRad() + Math.PI;
+                vel[i] = vel[i - 1].add(gravForce.multiply(length));
+                points[i] = points[i - 1].add(vel[i].multiply(length));
+            }
+
+            var distance = points[i].distance(planet);
+            if (distance > apoapsisDistance) {
+                if (playerDistance > distance) {
+                    apoapsis = position;
+                } else {
+                    apoapsis = points[i];
+                    apoapsisDistance = distance;
+                }
+            }
+            if (distance < periapsisDistance || periapsisDistance === 0) {
+                periapsis = points[i];
+                periapsisDistance = distance;
+            }
+        } else {
+            collisionCourse = true;
+            break;
+        }
+    }
+
+    apoapsisIcon.position.set(apoapsis.x, apoapsis.y);
+    periapsisIcon.position.set(periapsis.x, periapsis.y);
+
+    return points;
+}
+
+function lerp(start, end, percent) {
+    return (1 - percent) * start + percent * end;
+}
+
+function keyboard(value) {
+    let key = {};
+    key.value = value;
+    key.isDown = false;
+    key.isUp = true;
+    key.press = undefined;
+    key.release = undefined;
+
+    // Key down handler
+    key.downHandler = event => {
+        if (event.key === key.value) {
+            if (key.isUp && key.press) key.press();
+            key.isDown = true;
+            key.isUp = false;
+            event.preventDefault();
+        }
+    };
+
+    // Key up handler
+    key.upHandler = event => {
+        if (event.key === key.value) {
+            if (key.isDown && key.release) key.release();
+            key.isDown = false;
+            key.isUp = true;
+            event.preventDefault();
+        }
+    };
+
+    // Attach event listeners
+    const downListner = key.downHandler.bind(key);
+    const upListner = key.upHandler.bind(key);
+
+    window.addEventListener("keydown", downListner, false);
+    window.addEventListener("keyup", upListner, false);
+
+    // Detach event listners
+    key.unsubscribe = () => {
+        window.removeEventListener("keydown", downListner);
+        window.removeEventListener("keyup", upListner);
+    };
+
+    return key;
+}
+
+function gravity(position) {
+    let relativePosition = position.distanceTo(planet);
+    let distance = relativePosition.magnitude();
+    if (distance > planet.radius) {
+        let direction = relativePosition.normalized();
+        let gravForce = (planet.radius * 100 * 10) / (distance * distance);
+        return direction.multiply(gravForce);
+    } else {
+        return Vector.zero;
+    }
+}
+
+function clamp(num, min, max) {
+    return num <= min ? min : num >= max ? max : num;
+}
+
+function changeTimeWarpText(text) {
+    timeWarpText.alpha = 4;
+    timeWarpText.text = text;
+}
+
+function animate(object, animationName, loop = false, delta = 1) {
+    let animation = json.meta.frameTags.find(x => x.name === animationName);
+
+    if (object.lastAnimation !== animationName) {
+        object.frame = animation.from;
+    }
+
+    if (object.frame < animation.to + 1) {
+        let spriteNumber = object.frame - (object.frame % 1);
+        object.texture = id["Space Tilesheet " + spriteNumber + ".aseprite"];
+        let frameDuration =
+            json.frames["Space Tilesheet " + spriteNumber + ".aseprite"].duration;
+        object.frame += 1 / (((60 / delta) * frameDuration) / 1000);
+    } else if (loop) {
+        object.frame = animation.from;
+    } else {
+        object.frame = animation.to;
+    }
+    object.lastAnimation = animationName;
+}
+
+function createArray(length) {
+    var arr = new Array(length || 0),
+        i = length;
+
+    if (arguments.length > 1) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        while (i--) arr[length - 1 - i] = createArray.apply(this, args);
+    }
+
+    return arr;
+}
