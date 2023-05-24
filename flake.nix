35e93b7f6f5c3d16ce84a3f665e579c6337c6c44
Ryan Walker <ryan.walker@tombola.com>
2023-05-24 17:46:17 +0100

    nix run

--- flake.nix
+++ flake.nix
@@ -1,19 +1,24 @@
 {
-  outputs = { self, nixpkgs }:
-    let
-      allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-
-      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+  description = "kerbz";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
         pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    {
-      devShells = forAllSystems ({ pkgs }: {
-        default = pkgs.mkShell {
+      in
+      {
+        packages.default = pkgs.writeScriptBin "run" ''
+          npm i
+          npm run serve
+        '';
+
+        devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             nodejs
           ];
         };
-      });
-    };
+      }
+    );
 }
